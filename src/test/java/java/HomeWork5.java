fc3b8d5c03678f10779ac698a7063cd6e5fe9954
shevchuk92 <yevgeniy.shevchuk@ukr.net>
2019-09-19 11:35:02 +0300

    Initial commit

---
+++ src/test/java/java/HomeWork5.java
@@ -0,0 +1,73 @@
+package java;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class HomeWork5 {
+
+
+    public static class Driver {
+
+        private String arguments;
+
+        public enum Browser {
+            CHROME,
+            FIREFOX,
+            OPERA
+        }
+
+
+
+
+
+
+        public static WebDriver getDriver (Browser browser, String arguments) {
+            WebDriver driver;
+
+            {
+
+                switch (browser) {
+                    case CHROME:
+                        driver.ChromeDriver().setup ();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("start-fullscreen");
+                        WebDriver driverChrome = new ChromeDriver (options);
+                        return driverChrome;
+
+                    case FIREFOX:
+                        driver.firefoxdriver ().setup ();
+                        WebDriver driverFirefox = new FirefoxDriver ();
+                        driverFirefox.manage().window().maximize();
+                        return driverFirefox;
+
+
+                    case OPERA:
+                        driver.operadriver ().setup ();
+                        WebDriver driverOpera = new OperaDriver ();
+                        return driverOpera;
+
+
+
+                }
+                ;
+            }
+            return null;
+        };
+        public static void main (String[] args) {
+            String baseURL = "https://www.ukr.net/ua/";
+
+            for (Browser browser : Browser.values ()) {
+                System.out.println (browser.toString ());
+                WebDriver driver = Driver.getDriver (browser, "full-screen");
+                driver.get (baseURL);
+                driver.quit ();
+            }
+        }
+
+
+    };
+}
